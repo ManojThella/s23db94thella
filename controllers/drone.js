a1241b2b1df087ea8ddb5de44202130da33efbaa
ManojThella <manojthella98@gmail.com>
2023-04-14 11:46:04 -0500

    controller updated

--- controllers/drone.js
+++ controllers/drone.js
@@ -1,5 +1,5 @@
 var drone = require('../models/drone');
-/*// List of all drones
+/* List of all drones
 exports.drone_list = function(req, res) {
  res.send('NOT IMPLEMENTED: Drone list');
 };
@@ -19,9 +19,16 @@ exports.drone_list = async function(req, res) {
 
 
 // for a specific drone.
-exports.drone_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Drone detail: ' + req.params.id);
-};
+exports.drone_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await drone.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
 // Handle drone create on POST.
 exports.drone_create_post = async function(req, res) {
     console.log(req.body)
@@ -47,9 +54,27 @@ exports.drone_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: drone delete DELETE ' + req.params.id);
 };
 // Handle drone update form on PUT.
-exports.drone_update_put = function(req, res) {
+/*exports.drone_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: drone update PUT' + req.params.id);
-};
+};*/
+exports.drone_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await drone.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.type) toUpdate.type = req.body.type;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    if(req.body.use) toUpdate.use = req.body.use;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
 // VIEWS
 // Handle a show all view
 exports.drone_view_all_Page = async function(req, res) {
